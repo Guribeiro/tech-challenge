41538b6b7990d530a568b19ef7c921d666d36160
Gustavo Ribeiro <guribeirodeveloper@gmail.com>
2026-07-13 17:11:51 -0300

    chore(testes): adiciona vitest coverage

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,16 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/**/*.spec.ts',          // Exclui arquivos de testes unitários
+        'src/**/*.test.ts',          // Exclui arquivos de testes de integração
+        'src/**/tests/**',           // Exclui pastas de mocks/repositories em memória
+        'src/**/factories/**',       // Exclui as fábricas (factories) que criamos para os testes
+        'src/**/infra/**',           // Opcional: Se quiser ignorar a camada de infra/banco pura do coverage
+      ],
+    }
   }
 })
