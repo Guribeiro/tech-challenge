8c318923c2b26c54d72cfaa357b9dd5413fc0754
Gustavo Ribeiro <guribeirodeveloper@gmail.com>
2026-09-15 20:22:21 -0300

    chore(ci): melhorias no workflow

--- src/teste/helpers/reset-database.ts
+++ src/teste/helpers/reset-database.ts
@@ -1,27 +1,20 @@
-// test/helpers/reset-db.ts
 import { PrismaService } from '@/infra/database/prisma/prisma.service.js'
 
-let cachedFormattedTables: string | null = null
-
 export async function resetDatabase(prisma: PrismaService) {
-  // Busca o nome das tabelas apenas na primeira chamada da execução
-  if (!cachedFormattedTables) {
-    const tables = await prisma.$queryRaw<Array<{ tablename: string }>>`
-      SELECT tablename 
-      FROM pg_tables 
-      WHERE schemaname='public' 
-        AND tablename != '_prisma_migrations';
-    `
+  const tables = await prisma.$queryRaw<Array<{ tablename: string }>>`
+    SELECT tablename 
+    FROM pg_tables 
+    WHERE schemaname = 'public' 
+      AND tablename != '_prisma_migrations';
+  `
 
-    if (tables.length === 0) return
+  if (!tables || tables.length === 0) return
 
-    cachedFormattedTables = tables
-      .map(({ tablename }) => `"${tablename}"`)
-      .join(', ')
-  }
+  const formattedTables = tables
+    .map(({ tablename }) => `"${tablename}"`)
+    .join(', ')
 
-  // Trunca todas as tabelas usando a string já em cache
-  if (cachedFormattedTables) {
-    await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${cachedFormattedTables} RESTART IDENTITY CASCADE;`)
-  }
+  await prisma.$executeRawUnsafe(
+    `TRUNCATE TABLE ${formattedTables} RESTART IDENTITY CASCADE;`
+  )
 }
